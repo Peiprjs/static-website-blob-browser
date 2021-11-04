777c633b52d699c04ec60df4d3f88c7a40b553e4
Peipr <proca001@jvvgirona.eu>
2021-11-04 12:22:31 +0100

    succ ma welli

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ class App extends React.Component {
 
     // Custom links for various scenarios (handles blobs, directories and go back link)
     renderLink(blobName) {
-        var link = "https://mscssstatic.blob.core.windows.net/staticwebsite";
+        var link = "https://peiphylib.blob.core.windows.net/files/";
         if(blobName === "../")
         {
             link += "/index.html"
